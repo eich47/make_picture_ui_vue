fbcf75aff0021f304d2605429a7f69219065ff1a
Sergey <eich@tut.by>
2020-11-04 15:28:33 +0300

    перенос атрибутов на новые строки

--- assets/scripts.js
+++ assets/scripts.js
@@ -310,7 +310,10 @@ function ready() {
         <label class="col-sm-2 col-md-3 ">{{label}}</label>
         <div class="col-sm-10 col-md-9">
           <div class="custom-control custom-checkbox">
-              <input type="checkbox" name="user_texture" class="custom-control-input" id="texture">
+              <input type="checkbox"
+              name="user_texture"
+              class="custom-control-input"
+              id="texture">
               <label for="texture" class="custom-control-label">добавить</label>
           </div>
         </div>
