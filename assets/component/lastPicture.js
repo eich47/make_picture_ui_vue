994c5027c632e99e383bd625507047b45729036c
Sergey <eich@tut.by>
2020-11-14 13:37:40 +0300

    добавил текст о возможности загрузить ранее параметры раннее созданных картинок

--- assets/component/lastPicture.js
+++ assets/component/lastPicture.js
@@ -24,6 +24,12 @@ export default {
   },
   template:`
     <div v-if="countOfLastPicturesList > 0">
+    <div class="row no-gutters">
+        <h3 v-if="countOfLastPicturesList < 6" class="h5 col-12">Последние созданные картинки: </h3>
+        <h3 v-else class="h5 col-12">Последние 6 созданных картинок: </h3>
+        <p class="col-12 order-first">Вы также можете загрузить параметры ранее созданных картинок</p>
+    </div>
+    
     <div class="row">
         <last-options-picture
             v-for="options in lastPictureList"
