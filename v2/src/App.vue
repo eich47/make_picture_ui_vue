916a01005d8180d989119e5f688df1752994d29d
Sergey <eich@tut.by>
2020-11-16 21:46:26 +0300

    базовая разметка

--- v2/src/App.vue
+++ v2/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-
+    <b-container class="mt-3">
+      <h1 class="h3 text-center mb-3">Создание картинки по параметрам</h1>
+      <b-row>
+        <b-col md="5"></b-col>
+        <b-col></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
