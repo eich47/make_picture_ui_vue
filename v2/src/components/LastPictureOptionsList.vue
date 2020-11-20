aac25cabf6b6db095ec2eff0573273a4aa0cf493
Sergey <eich@tut.by>
2020-11-21 01:55:39 +0300

    стили: для блока последних созданных картинок

--- v2/src/components/LastPictureOptionsList.vue
+++ v2/src/components/LastPictureOptionsList.vue
@@ -1,11 +1,21 @@
 <template>
-    <b-row>
-        <LastPictureOptions
-                v-for="options in lastOptionsList"
-                :key="options.id"
-                :options="options"
-        ></LastPictureOptions>
-    </b-row>
+    <div>
+        <b-row>
+            <b-col cols="12">
+                <h3 class="h5">Последние созданные картинки: </h3>
+            </b-col>
+            <b-col cols="12" class="order-first mt-3">
+                <p>Вы также можете загрузить параметры ранее созданных картинок</p>
+            </b-col>
+        </b-row>
+        <b-row class="mt-3">
+            <LastPictureOptions
+                    v-for="options in lastOptionsList"
+                    :key="options.id"
+                    :options="options"
+            ></LastPictureOptions>
+        </b-row>
+    </div>
 </template>
 
 <script>
