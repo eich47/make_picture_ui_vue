f1cd9bc0c0d6a5f6602db5625ccf5ec0dff8ada9
Sergey <eich@tut.by>
2020-11-23 13:14:24 +0300

    анимация появления загруженной картинки и информации для ее сохранения

--- v2/src/components/PictureBlock.vue
+++ v2/src/components/PictureBlock.vue
@@ -1,31 +1,37 @@
 <template>
-    <div>
+    <div :class="{ marginToNotJump: this.$store.getters.getIsUserSendForm}">
         <div class="block-height d-flex justify-content-center align-items-md-center" >
 
             <PictureStubProcess
                     v-if="isProcess"
             />
-
+            <transition name="picture-loaded">
             <PictureLoaded
                     v-if="isLoaded"
                     :src-img="getUrl"
             >
 
             </PictureLoaded>
+            </transition>
         </div>
 
         <template v-if="isLoaded">
+        <transition name="show-save-info">
         <p class="mt-3">
             Чтобы сохранить картинку, нажмите на ней правой кнопкой мыши
             и выберите подходящий пункт меню.
         </p>
+        </transition>
+        <transition name="show-save-info">
         <p>
             Вы также можете открыть картинку в новой вкладке кликнув
             по ссылке <a href="#"
                          @click.prevent="onClick"
                       >открыть</a>
         </p>
+        </transition>
         </template>
+
     </div>
 
 
@@ -65,4 +71,41 @@
     .block-height{
         height: 370px;
     }
+
+    /*появление*/
+    .picture-loaded-enter{
+        opacity: 0;
+    }
+    .picture-loaded-enter-active{
+        transition: opacity .5s ease;
+    }
+    .picture-loaded-enter-to{
+        opacity: 1;
+    }
+    /*скрытие*/
+    .picture-loaded-leave{
+        opacity: 1;
+    }
+    .picture-loaded-leave-active{
+        transition: opacity .1s linear;
+    }
+    .picture-loaded-leave-to{
+        opacity: 0;
+    }
+
+    /*анимация для текста*/
+    .show-save-info-enter{
+        opacity: 0;
+    }
+    .show-save-info-enter-active{
+        transition: opacity .5s ease;
+    }
+    .show-save-info-enter-to{
+        opacity: 1;
+    }
+
+    .marginToNotJump{
+        margin-top: 40px;
+    }
+
 </style>
